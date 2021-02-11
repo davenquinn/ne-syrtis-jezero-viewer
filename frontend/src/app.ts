6f18661783a4b25654c41fc9cb31dee6d9024caf
Daven Quinn <dev@davenquinn.com>
2021-02-11 00:01:55 -0600

    Position hashes are settable

--- frontend/src/app.ts
+++ frontend/src/app.ts
@@ -3,6 +3,7 @@ import { useRef, useState } from "react";
 import { Provider, useSelector } from "react-redux";
 import { createStore } from "redux";
 import { CTXLayer, HillshadeLayer, SyrtisTerrainProvider } from "./layers";
+import { setHashString } from "@macrostrat/ui-components/lib/esm/util/query-string";
 
 import CesiumViewer, { DisplayQuality } from "cesium-viewer";
 import styles from "./main.styl";
@@ -13,16 +14,58 @@ import {
   reducer,
   ActiveMapLayer,
   createInitialState,
+  GlobeAction,
+  GlobeState,
 } from "cesium-viewer/actions";
 import { PositionListEditor, CopyPositionButton } from "./editor";
 import positions from "./positions.js";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { flyToParams } from "cesium-viewer/position";
+import { flyToParams, CameraParams } from "cesium-viewer/position";
 import mainText from "../text/output/main.html";
 import viewerText from "../text/output/viewer.html";
 import changelogText from "../text/output/changelog.html";
 
 const h = hyperStyled(styles);
+
+interface PositionHashParams {
+  x: string;
+  y: string;
+  z?: string;
+  e?: string;
+  a?: string;
+}
+
+function buildPositionHash(pos: CameraParams): PositionHashParams {
+  let res = {
+    x: pos.longitude.toFixed(3),
+    y: pos.latitude.toFixed(3),
+    z: pos.height.toFixed(0),
+  };
+
+  const elevationAngle = Math.round(90 + pos.pitch);
+  if (elevationAngle != 0) {
+    res.e = elevationAngle.toFixed(0);
+  }
+
+  let az = Math.round(pos.heading);
+  if (az >= 360) az -= 360;
+  if (az < 0) az += 360;
+  if (az != 0) res.a = az.toFixed(0);
+
+  return res;
+}
+
+const newReducer = (state: GlobeState, action: GlobeAction) => {
+  switch (action.type) {
+    case "set-camera-position":
+      // Hook into the camera position setter to change viewer hash
+      let posHash = buildPositionHash(action.value.camera);
+      setHashString(posHash);
+      return reducer(state, action);
+    default:
+      return reducer(state, action);
+  }
+};
 
 const initialState = createInitialState({
   positions,
@@ -38,7 +81,7 @@ const initialState = createInitialState({
     : DisplayQuality.High,
 });
 let store = createStore(
-  reducer,
+  newReducer,
   initialState,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
