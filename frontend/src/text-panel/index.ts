237f5e55b2951b9713bec23e211f06bc3eb3fa2b
Daven Quinn <dev@davenquinn.com>
2021-03-06 00:53:44 -0600

    Added click handler

--- frontend/src/text-panel/index.ts
+++ frontend/src/text-panel/index.ts
@@ -1,6 +1,6 @@
 import { hyperStyled } from "@macrostrat/hyper";
 import styles from "./main.styl";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState, useCallback } from "react";
 import { PositionListEditor } from "../editor";
 import positions from "../positions.js";
 import { useDispatch, useSelector } from "react-redux";
@@ -40,7 +40,7 @@ const TextPanel = (props) => {
 
   // Restyle selection on change
   const setSelectionStyles = () => {
-    if (ref.current == null) return;
+    if (ref?.current == null) return;
     // Set up "active" class name for selection
     const elements = ref.current.querySelectorAll(`[data-location]`);
     for (const e of elements) {
@@ -50,7 +50,7 @@ const TextPanel = (props) => {
       });
     }
   };
-  useEffect(setSelectionStyles, [currentLocation, ref.current, html]);
+  useEffect(setSelectionStyles, [currentLocation, ref, html]);
 
   const onScroll = (evt) => {
     const scrollPos = scrollParentRef.current.scrollTop;
@@ -81,9 +81,24 @@ const TextPanel = (props) => {
     scrollParentRef.current.onscroll = onScroll;
   }, [offsetCache]);
 
+  // CLICK HANDLERS - COULD NOT GET THIS TO WORK QUITE RIGHT
+  // Navigate by click or arrow keys
+  let navigateTo = useCallback(
+    function () {
+      const elName = this.getAttribute("data-location");
+      // Dispatch the location
+      dispatch({
+        type: "fly-to-named-location",
+        value: elName,
+      });
+      //scrollParentRef.current.scrollTop = loc.offset - 240;
+    },
+    [offsetCache]
+  );
+
   // Set up initial handlers, etc.
   useEffect(() => {
-    if (ref.current == null || scrollParentRef.current == null) return;
+    if (ref?.current == null || scrollParentRef.current == null) return;
 
     console.log("Setting up positions cache");
 
@@ -95,52 +110,41 @@ const TextPanel = (props) => {
     }, 1000);
 
     setSelectionStyles();
-  }, [ref.current, html]);
+
+    const elements = ref.current.querySelectorAll(`[data-location]`);
+    for (const e of elements) {
+      const elName = e.getAttribute("data-location");
+      if (elName != null) e.addEventListener("click", navigateTo);
+    }
+  }, [ref, html]);
 
   const __html = typeset(html);
+
+  const keyDownHandler = (event) => {
+    switch (event.which) {
+      case 38: // up
+        return navigateTo(offsetCache[currentIndex + 1]);
+      case 40: // down
+        return navigateTo(offsetCache[currentIndex - 1]);
+    }
+    event.preventDefault(); // prevent the default action (scroll / move caret)
+  };
+
+  // Set up initial handlers, etc.
+  useEffect(() => {
+    // Bind up and down arrow keys to navigate items
+    document.addEventListener("keydown", keyDownHandler);
+    // Return a function to destroy event handlers
+    return () => {
+      document.removeEventListener("keydown", keyDownHandler);
+    };
+  }, [ref, offsetCache, currentLocation]);
 
   return h("div.text-panel", { ref }, [
     //h("div.scroll-indicator"),
     h("div.text", { dangerouslySetInnerHTML: { __html } }),
   ]);
 };
-
-/*
-// CLICK HANDLERS - COULD NOT GET THIS TO WORK QUITE RIGHT
-// Navigate by click or arrow keys
-const navigateTo = (loc) => {
-  console.log(loc);
-  if (loc == null || loc.name == currentLocation) return;
-  setAllowScroll(false);
-  // Dispatch the location
-  dispatch({
-    type: "fly-to-named-location",
-    value: loc.name,
-  });
-  scrollParentRef.current.scrollTop = loc.offset - 240;
-  setAllowScroll(true);
-};
-
-const keyDownHandler = (event) => {
-  switch (event.which) {
-    case 38: // up
-      return navigateTo(offsetCache[currentIndex + 1]);
-    case 40: // down
-      return navigateTo(offsetCache[currentIndex - 1]);
-  }
-  event.preventDefault(); // prevent the default action (scroll / move caret)
-};
-
-// Set up initial handlers, etc.
-useEffect(() => {
-  // Bind up and down arrow keys to navigate items
-  document.addEventListener("keydown", keyDownHandler);
-  // Return a function to destroy event handlers
-  return () => {
-    document.removeEventListener("keydown", keyDownHandler);
-  };
-}, [ref.current, offsetCache, currentLocation]);
-*/
 
 const Sidebar = () => {
   return h("div.left", [
